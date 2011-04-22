74c5c28265ac69676c3f9997a948e8437f59b26e
Bobby Wilson <bobbywilson0@gmail.com>
2011-04-22 07:12:57 -0600

    initial push

---
+++ chapter_1/1-3.scm
@@ -0,0 +1,10 @@
+(define (sum-sq x y)
+  (+ (* x x)(* y y)))
+
+(define (sum-sq-larger-nums a b c)
+  (cond ((and (>= a b) (>= b c)) (sum-sq(a b))
+	((and (>= b c) (>= c a)) (sum-sq(b c))
+	((and (>= a b) (>= c b)) (sum-sq(a c)))))
+
+
+
