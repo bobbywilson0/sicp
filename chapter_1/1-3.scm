ccf3ddc0cf7b4e0389a659c26209d9e915cba0ae
Bobby Wilson <bobbywilson0@gmail.com>
2011-04-22 21:52:35 -0600

    fixed square larger sums function

--- chapter_1/1-3.scm
+++ chapter_1/1-3.scm
@@ -2,9 +2,9 @@
   (+ (* x x)(* y y)))
 
 (define (sum-sq-larger-nums a b c)
-  (cond ((and (>= a b) (>= b c)) (sum-sq(a b))
-	((and (>= b c) (>= c a)) (sum-sq(b c))
-	((and (>= a b) (>= c b)) (sum-sq(a c)))))
-
-
+  (cond 
+    ((and (>= a b)(>= b c)) (sum-sq a b))
+    ((and (>= b c)(>= c a)) (sum-sq b c))
+    ((and (>= a b)(>= c b)) (sum-sq a c))
+    ((and (>= b a)(>= c a)) (sum-sq b c))))
 
