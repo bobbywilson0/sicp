9054ae833f7045320a4cfb5cd4e001ebf8a33163
Bobby Wilson <bobbywilson0@gmail.com>
2011-04-22 22:41:10 -0600

    added 1-4

---
+++ chapter_1/1-4.scm
@@ -0,0 +1,4 @@
+;The evaluted if statement returns a plus or minus operator that is then 
+;applied to 'b'. The expression then uses subtraction if 'b' is negative
+;to or addition if 'b' is positive affectively making 'b' the absolute
+;value.
